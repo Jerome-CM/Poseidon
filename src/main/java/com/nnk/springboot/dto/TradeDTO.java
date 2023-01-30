745642347df38dff615369053f3a2a5a66bb09fd
Bouteveille jerome <formation@community-technologie.fr>
2023-01-30 21:00:35 +0100

    Add attribute

--- src/main/java/com/nnk/springboot/dto/TradeDTO.java
+++ src/main/java/com/nnk/springboot/dto/TradeDTO.java
@@ -2,6 +2,8 @@ package com.nnk.springboot.dto;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 public class TradeDTO {
 
@@ -9,4 +11,21 @@ public class TradeDTO {
     private String account;
     private String type;
     private Double buyQuantity;
+    private Double sellQuantity;
+    private Double buyPrice;
+    private Double sellPrice;
+    private String benchmark;
+    private Timestamp tradeDate;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
 }
