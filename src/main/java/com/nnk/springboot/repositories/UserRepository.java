e8ae02bfdee1ec43c5be25c903a7f55f426f83ef
Bouteveille jerome <formation@community-technologie.fr>
2023-01-30 21:03:01 +0100

    Update a User without password changed

--- src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -3,12 +3,18 @@ package com.nnk.springboot.repositories;
 import com.nnk.springboot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
     public User findByUsername(String username);
+
+    @Modifying
+    @Transactional
+    @Query(value="UPDATE users SET username = ?1, fullname = ?2, role = ?3 WHERE id = ?4", nativeQuery = true)
+    public void saveWithoutPassword(String username, String fullname, String role, long id);
 
 }
