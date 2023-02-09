d3a73228d5ab1bb3fa3339cffe8c85042eead91a
Bouteveille jerome <formation@community-technologie.fr>
2023-02-09 21:27:13 +0100

    Init custom error or success

--- src/main/java/com/nnk/springboot/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/nnk/springboot/config/CustomAuthenticationFailureHandler.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +14,6 @@ import java.io.IOException;
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
     private static final Logger logger = LogManager.getLogger(CustomAuthenticationFailureHandler.class);
-
 
 
     @Override
--- src/main/java/com/nnk/springboot/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/nnk/springboot/config/CustomAuthenticationSuccessHandler.java
@@ -1,29 +1,61 @@
 package com.nnk.springboot.config;
 
-import com.nnk.springboot.dto.response.ResponseDTO;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpSession;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.jwtConfig.JwtTokenUtil;
+import com.nnk.springboot.services.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     private static final Logger logger = LogManager.getLogger(CustomAuthenticationSuccessHandler.class);
+    @Autowired
+    private JwtTokenUtil jwtTest;
 
+    @Autowired
+    private  UserService userService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         logger.info("--- Method onAuthenticationSuccess ---");
-        response.sendRedirect("/");
+        /*Take the mail in the session after success login*/
+
+        HttpSession session = request.getSession();
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        User user = userService.getUserByUsername(username);
+
+        if(session.getAttribute("token") == null){
+            logger.info("Create a new token because is null in the session");
+            String token = null;
+            try {
+                token = jwtTest.createAuthenticationToken(username);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            session.setAttribute("token", token);
+            session.setAttribute("role", user.getRole());
+        }
+        logger.info("Token in session : {}", session.getAttribute("token"));
+
+        if(user.getRole().equals("ADMIN")){
+            response.sendRedirect("/admin/home");
+        }else{
+            response.sendRedirect("/");
+        }
     }
 }
