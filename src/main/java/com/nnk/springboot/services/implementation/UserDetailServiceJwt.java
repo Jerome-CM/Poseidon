79e77c36ee7153d93886cd845b2bb920ca6b5793
Bouteveille jerome <formation@community-technologie.fr>
2023-02-09 21:34:38 +0100

    Refactor

--- src/main/java/com/nnk/springboot/services/implementation/UserDetailServiceJwt.java
+++ src/main/java/com/nnk/springboot/services/implementation/UserDetailServiceJwt.java
@@ -2,8 +2,6 @@ package com.nnk.springboot.services.implementation;
 
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,8 +13,6 @@ public class UserDetailServiceJwt implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
-    private static final Logger logger = LogManager.getLogger(UserDetailServiceJwt.class);
 
     /**
      * @param username :
@@ -31,8 +27,6 @@ public class UserDetailServiceJwt implements UserDetailsService {
             throw new UsernameNotFoundException(username);
         }
         /* load username, password and Authorities in a User Spring */
-        UserDetails userAuth = org.springframework.security.core.userdetails.User.withUsername(user.getUsername()).password(user.getPassword()).authorities(user.getRole()).build();
-        logger.info("Connexion User : {}", userAuth);
-        return userAuth;
+        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername()).password(user.getPassword()).authorities(user.getRole()).build();
     }
 }
