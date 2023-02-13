a6c3798037960693972687897c549f2cc90f78ce
Bouteveille jerome <formation@community-technologie.fr>
2023-02-13 22:51:04 +0100

    Add profil values

--- src/test/java/com/nnk/springboot/CustomSecurityMockMvcRequestPostProcessors.java
+++ src/test/java/com/nnk/springboot/CustomSecurityMockMvcRequestPostProcessors.java
@@ -1,2 +1,16 @@
-package com.nnk.springboot;public class CustomSecurityMockMvcRequestPostProcessors {
+package com.nnk.springboot;
+
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
+public class CustomSecurityMockMvcRequestPostProcessors {
+
+    public static RequestPostProcessor adminValue() {
+        return user("admin").password("azerty").roles("ADMIN");
+    }
+
+    public static RequestPostProcessor userValue() {
+        return user("teddou").password("azerty").roles("USER");
+    }
 }
