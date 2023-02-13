9b99aa8fbcfb4e2a82890a9cf770ed7eda55d418
Bouteveille jerome <formation@community-technologie.fr>
2023-02-13 22:55:18 +0100

    Delete pattern annotation

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
@@ -17,7 +16,6 @@ public class User {
     @Column(unique = true)
     private String username;
     @NotBlank(message = "Password is mandatory")
-    @Pattern(regexp = "^(?=.[A-Z])(?=.\\d)(?=.[@$!%#?&])[A-Za-z\\d@$!%#?&]{8,}$", message = "At least one capital letter, 8 characters minimum, at least one number and one symbol (@$!%#?&)")
     private String password;
     @NotBlank(message = "FullName is mandatory")
     private String fullname;
