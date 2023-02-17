227569fca08bb302729e06fcb001ba086d838b5b
Bouteveille jerome <formation@community-technologie.fr>
2023-02-17 12:48:39 +0100

    Init class

--- src/main/java/com/nnk/springboot/Utility/PasswordValidate.java
+++ src/main/java/com/nnk/springboot/Utility/PasswordValidate.java
@@ -1,2 +1,17 @@
-package com.nnk.springboot.Utility;public class passwordValidate {
+package com.nnk.springboot.Utility;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidate {
+
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$";
+
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+    public static boolean isValid(final String password) {
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
 }
