1c6b8ab4d4e6979de5af5de4953c2340ed52c2e4
Bouteveille jerome <formation@community-technologie.fr>
2023-02-20 22:40:52 +0100

    Delete message in session

--- src/main/java/com/nnk/springboot/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/nnk/springboot/config/CustomAuthenticationFailureHandler.java
@@ -1,10 +1,13 @@
 package com.nnk.springboot.config;
 
+import com.nnk.springboot.dto.response.ResponseDTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,9 +20,8 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse responseHttp, AuthenticationException exception) throws IOException, ServletException {
         logger.info("--- Method onAuthenticationFailure ---");
-        request.getSession().setAttribute("error", "Identifiant ou mot de passe incorrect");
-        response.sendRedirect("/app/login?error=true");
+        responseHttp.sendRedirect("/app/login");
     }
 }
